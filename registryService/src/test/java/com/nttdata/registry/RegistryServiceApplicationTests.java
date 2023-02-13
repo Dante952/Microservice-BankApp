1dc9b48dd7ae73a1d1582405ee81cc4fe7067544
Dante952 <dantetinez20@gmail.com>
2023-02-13 11:17:32 -0500

    registry config

---
+++ registryService/src/test/java/com/nttdata/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.nttdata.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

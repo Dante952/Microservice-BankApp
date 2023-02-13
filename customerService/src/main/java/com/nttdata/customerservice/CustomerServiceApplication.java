b64395faa324ef24d5b9fa51c7956528db50adc0
Dante952 <dantetinez20@gmail.com>
2023-02-13 12:32:44 -0500

    Customer Registry

--- customerService/src/main/java/com/nttdata/customerservice/CustomerServiceApplication.java
+++ customerService/src/main/java/com/nttdata/customerservice/CustomerServiceApplication.java
@@ -2,6 +2,7 @@ package com.nttdata.customerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Microservice in charge of managing bank clients, such as business and personal.
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(CustomerServiceApplication.class, args);
